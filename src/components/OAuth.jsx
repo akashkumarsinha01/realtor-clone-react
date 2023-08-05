c8d37ea43ee1e6a96a55e28522796fa0827e14cb
akashkumarsinha01 <akashkumarsinha01@gmail.com>
2023-08-06 02:50:16 +0530

    Add my listings sections to the profile page and create the listingitem component

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,26 +1,26 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
-import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore'
-import React from 'react'
-import {FcGoogle} from "react-icons/fc"
-import { db } from '../firebase'
-import { useNavigate } from 'react-router-dom'
-import { toast } from 'react-toastify'
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import { db } from "../firebase";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 export default function OAuth() {
-  const navigate = useNavigate
-  async function onGoogleClick(){
+  const navigate = useNavigate();
+  async function onGoogleClick() {
     try {
-      const auth = getAuth()
-      const provider = new GoogleAuthProvider()
-      const result = await signInWithPopup(auth,provider)
-      const user = result.user
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
 
       //check for the user in db
 
-      const docRef = doc(db,"users",user.uid)
-      const docSnap = await getDoc(doc);
-      
-      if(!docSnap.exists()){
-        await setDoc(docRef,{
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
           name: user.displayName,
           email: user.email,
           timestamp: serverTimestamp(),
@@ -28,13 +28,18 @@ export default function OAuth() {
       }
       navigate("/");
     } catch (error) {
+      console.log(error);
       toast.error("Could not authorize with google");
     }
   }
   return (
-    <button type="button"onClick={onGoogleClick} className='flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded'>
-        <FcGoogle className='text-2xl bg-white rounded mr-2'/>
-        Continue with google
+    <button
+      type="button"
+      onClick={onGoogleClick}
+      className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded"
+    >
+      <FcGoogle className="text-2xl bg-white rounded mr-2" />
+      Continue with google
     </button>
-  )
+  );
 }
