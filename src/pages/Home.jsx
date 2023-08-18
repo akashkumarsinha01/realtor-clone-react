95436762cdad07f341a1fe1eba1529252a940de1
akashkumarsinha01 <akashkumarsinha01@gmail.com>
2023-08-18 23:40:35 +0530

    Create a slider component and add it to the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Slider from "../components/Slider";
+import { db } from "../firebase";
+import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
 
 export default function Home() {
   return (
     <div>
-      Home
+      <Slider />
     </div>
-  )
+  );
 }
